bfabf1c85e2161d7adc387e624f4c1ba8d158783
Issam Sakr <issam.sakr@timefonds.com>
2024-02-25 20:17:59 +0100

    test config file name changed

--- install.sh
+++ install.sh
@@ -156,4 +156,4 @@ for ((i = 0; i < total_steps; i++)); do
 done
 
 echo "" # Move to the next line after the progress bar is complete
-echo "Installation $(cat $CLI_WORKDIR/VERSION) complete!"
+echo "${cli_name} $(cat $CLI_WORKDIR/VERSION) Installed ✅"
